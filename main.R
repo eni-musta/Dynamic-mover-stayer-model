da60a1b6ba35a81c96848a6d6a90704f2f815305
Eni Musta <63855216+eni-musta@users.noreply.github.com>
2025-04-09 21:08:43 +0200

    Update main.R

--- main.R
+++ main.R
@@ -11,7 +11,7 @@ TVC=array(NA,dim=c(n,K+1,m))
 TVC[,,1]=as.matrix(Z1)
 TVC[,,2]=as.matrix(Z2)
 
-approx_order=3  #order of polynomial approximation of time varying intercept (if 0 it means constant intercept) for the transition probabilities 1->2, 1->3
+approx_order=0  #order of polynomial approximation of time varying intercept (if 0 it means constant intercept) for the transition probabilities 1->2, 1->3
 d1=dim(X)[2]
 d2=(m+approx_order)
 initial_par= rep(0,2*d2+3*d1) # initial parameter for the optimization procedure
@@ -32,7 +32,7 @@ j=1
 while(j<=B){
     index_sample=sample(1:n,size=n,replace = TRUE)
     tryCatch({
-      DMS_fit=DMS(X[index_sample,-1],TVC[index_sample,,],obs_State[index_sample],obs_time[index_sample],optim_method="BFGS",maxit=100,initial_par=est)
+      DMS_fit=DMS(baseline_cov=X[index_sample,-1],TVC=TVC[index_sample,,],approx_order=approx_order,obs_State=obs_State[index_sample],obs_time=obs_time[index_sample],optim_method="BFGS",maxit=100,initial_par=est)
       est_boot[j,]=c(DMS_fit$alpha_0,DMS_fit$beta_12,DMS_fit$beta_13,DMS_fit$gamma_12,DMS_fit$gamma_13)
     }, error = function(e){j<<- j-1}, finally ={j<<- j+1})
   }
@@ -44,5 +44,5 @@ U=est+boot_sd*qnorm(1-conf_lev/2)
 # results dataframe
 results=data.frame(cbind(est,L,U,boot_sd))
 colnames(results)=c("par_est","Lower bound CI", "Upper bound CI","standard error")
-
+row.names(result)=c("alpha_0","alpha_1","alpha_2","beta12_0","beta12_1","beta12_2","beta13_0","beta13_1","beta13_2","gamma12_1","gamma12_2","gamma13_1","gamma13_2")
 
